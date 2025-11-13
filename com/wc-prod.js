586881d577f9b9c41bf248000601f17fb1d2b1d7
chinbayar0220 <chinbayarchinba3@gmail.com>
2025-11-13 14:58:31 +0800

    hannaa

---
+++ com/wc-prod.js
@@ -0,0 +1,25 @@
+class WcProd extends HTMLElement {
+    constructor() {
+        super();
+        // 
+    }
+
+    connectedCallback() {
+        // 
+    }
+
+    disconnectedCallback() {
+        // 
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        // 
+    }
+
+    adoptedCallback() {
+        // 
+    }
+
+}
+
+window.customElements.define('wc-prod', WcProd);
