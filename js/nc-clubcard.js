34cdd3a05d4ba36f87b5fea5c9c2dcd7fc3ea28f
enkhjingan <enhjing72@gmail.com>
2025-12-04 11:56:34 +0800

    button holboh

--- js/nc-clubcard.js
+++ js/nc-clubcard.js
@@ -4,25 +4,39 @@ class NcClubcard extends HTMLElement {
     }
 
     connectedCallback() {
-        
         const clubname = this.getAttribute('cname') || 'Club name';
         const description = this.getAttribute('desc') || 'Мэдээллийн технологийн чиглэлээр үйл ажиллагаа явуулдаг бөгөөд оюутнуудын мэдлэгийг дээшлүүлэх, практик ур чадвар олгох, инновац бүтээхэд чиглэсэн үйл ажиллагаа явуулдаг';
         const button1 = this.getAttribute('btn1') || 'Элсэх';
         const button2 = this.getAttribute('btn2') || 'Дэлгэрэнгүй';
-
+        const imgSrc = this.getAttribute('img') || this.getAttribute('logo') || 'images/event.png';
+        const imgAlt = this.getAttribute('img-alt') || clubname;
+        const clubId = this.getAttribute('club-id') || this.getAttribute('data-index') || '1';
 
         this.innerHTML=`
         <article class="club_card">
-                    <div class="club_name">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDsCK1U_HGMYYAFqOOPaqu71kFkUg9ywSSGQ&amp;s" class="sFlh5c FyHeAf" alt="Hackum Students Club" jsname="JuXqh" style="max-width: 1292px; width: 58px; height: 58px; margin: 0px;" data-ilt="1759989884696">
-                        <h3>${clubname}</h3>
-                    </div>
-                    <p class="desc">${description}</p>
-                    <div class="buttons">
-                        <button class="btn1">${button1}</button>
-                        <button class="btn2">${button2}</button>
-                    </div>
-                </article>`;
+            <div class="club_name">
+                <img src="${imgSrc}" alt="${imgAlt}" class="club-card-img" style="width:58px;height:58px;object-fit:cover;border:1px solid var(--color-gray);box-shadow:0px 4px 16px rgba(0,0,0,0.06);">
+                <h3>${clubname}</h3>
+            </div>
+            <p class="desc">${description}</p>
+            <div class="buttons">
+                <button class="btn1">${button1}</button>
+                <button class="btn2" data-club-id="${clubId}">Дэлгэрэнгүй</button>
+            </div>
+        </article>`;
+        
+        const btn2 = this.querySelector('.btn2');
+        if (btn2) {
+            btn2.addEventListener('click', (e) => {
+                e.stopPropagation();
+                const id = btn2.getAttribute('data-club-id');
+                if (window.Router) {
+                    window.Router.navigate(`/club/${id}`);
+                } else {
+                    window.location.hash = `#/club/${id}`;
+                }
+            });
+        }
     }
 
     disconnectedCallback() {
